ea5398ab81ef2a5995f4521816dd4d6fc136090d
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2020-06-30 14:16:34 -0500

    Fix the shared static memory stream between tests

--- Block.Core.Tests/BlockAccessorTests.fs
+++ Block.Core.Tests/BlockAccessorTests.fs
@@ -6,27 +6,30 @@ module BlockAccessorTests
     open Block.Core
 
     module TestApi = 
-        let private stream = new System.IO.MemoryStream()
-        let private fileLocator = HostsFileWebBlockAccessor.Stream stream
-        let BlockSite = HostsFileWebBlockAccessor.blockSite fileLocator
-        let UnblockSite = HostsFileWebBlockAccessor.unblockSite fileLocator
-        let ListBlockedSites =  HostsFileWebBlockAccessor.listBlockedSites fileLocator
-        let GetRawHostFile () = 
-            stream.Position <- int64 0 
-            (new System.IO.StreamReader(stream)).ReadToEnd()
+        // alternately, I could create a class that internally manages the partial application of the host file, but that isn't very functional
+        let MockHostFile () = HostsFileWebBlockAccessor.Stream (new System.IO.MemoryStream())
+        let BlockSite = HostsFileWebBlockAccessor.blockSite 
+        let UnblockSite = HostsFileWebBlockAccessor.unblockSite 
+        let ListBlockedSites =  HostsFileWebBlockAccessor.listBlockedSites 
+        let GetRawHostFile fileLocator = 
+            match fileLocator with 
+            | HostsFileWebBlockAccessor.Stream s -> (s.Position <- int64 0; (new System.IO.StreamReader(s)).ReadToEnd())
+            | _ -> raise (NotImplementedException())
 
     // expecto mentions https://github.com/SwensenSoftware/unquote, but I don't quite understand the value prop yet
     [<Tests>]
     let ``My test`` =
         testList "Block accessor" [
             test "List Blocked Sites When None Blocked" {
-                let blockedSites = TestApi.ListBlockedSites ()    
+                let hostFile = TestApi.MockHostFile ();
+                let blockedSites = TestApi.ListBlockedSites hostFile ()    
                 Expect.isEmpty blockedSites "Blocked sites should be empty"
             }
             test "Block empty" {
+                let hostFile = TestApi.MockHostFile ();
                 let site = { Url = "" }
-                TestApi.BlockSite site
-                let blockedSites = TestApi.ListBlockedSites ()   
+                TestApi.BlockSite hostFile site
+                let blockedSites = TestApi.ListBlockedSites hostFile ()   
                 Expect.sequenceEqual (Seq.empty) blockedSites "Site not blocked??"
             }
             test "Unblock When Not Blocked" {
@@ -39,17 +42,19 @@ module BlockAccessorTests
                 raise (NotImplementedException())
             }
             test "Block Single" {
+                let hostFile = TestApi.MockHostFile ();
                 let site = { Url = "www.meow.com" }
-                TestApi.BlockSite site
-                let blockedSites = TestApi.ListBlockedSites ()
+                TestApi.BlockSite hostFile site
+                let blockedSites = TestApi.ListBlockedSites hostFile ()
                 Expect.sequenceEqual blockedSites (seq { site }) "Site not blocked??"
             }
             test "Sequential Blocks" {
+                let hostFile = TestApi.MockHostFile ();
                 let site1 = { Url = "www.meow.com" }
-                TestApi.BlockSite site1
+                TestApi.BlockSite hostFile site1
                 let site2 = { Url = "www.pat.com" }
-                TestApi.BlockSite site2
-                let blockedSites = TestApi.ListBlockedSites ()
+                TestApi.BlockSite hostFile site2
+                let blockedSites = TestApi.ListBlockedSites hostFile ()
 
                 Expect.sequenceEqual blockedSites (seq { site1; site2 }) "Site not blocked??"
             }
